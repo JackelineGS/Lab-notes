006335b42ed8146436a17a33f1aa993dbc01c20f
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-10-20 14:30:03 -0500

    We create React project

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import TaskList from './components/TaskList'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<TaskList/>}> 
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
---
+++ client/src/components/TaskForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function 
+TaskForm() {
+  return (
+    <div>
+        TaskForm
+    </div>
+  )
+}
