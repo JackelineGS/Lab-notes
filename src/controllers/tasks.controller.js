675510a85d288e368df96245d4b67f506a94f5f8
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-10-19 22:43:24 -0500

    We add the function to control the errors

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,20 +1,20 @@
 const pool = require('../db')
 
 // Traer todas las tareas
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (req, res, next) => {
 //res.send('retrieven a list of tasks')
 
-try { 
-const allTasks = await pool.query('SELECT * FROM task')
-res.json(allTasks.rows)
-} catch(error) {
-console.log(error.message)    
+try {
+  const allTasks = await pool.query("SELECT * FROM task");
+  res.json(allTasks.rows);
+} catch (error) {
+  next(error);
 }
 
 };
 
 // Traer una tarea 
-const getTask = async (req, res) => {
+const getTask = async (req, res, next) => {
 // Obtengo el id desde req.params    
     try {
         const {id} = req.params
@@ -26,13 +26,13 @@ const getTask = async (req, res) => {
     });
     res.json(result.rows[0]); //devuelve la tarea que has encontrado
     } catch(console) {
-        console.log(error.message);
+        next(error);
     }
 };
 
 // Crear una tarea 
 // para ver que datos me estan enviando voy a usar req.body, nos permite conocer la información que nos envían las aplicaciones cliente
- const createTask = async (req, res) => {
+ const createTask = async (req, res, next) => {
     const { title, description } = req.body;
     
     try {
@@ -43,37 +43,46 @@ const getTask = async (req, res) => {
     res.json(result.rows[0]);
 
     } catch(error) {
-    console.log(error.message);
-    res.json({ error: error.message })
+    // res.json({ error: error.message })
+    // al colocar error entre parentensis se dirige a la función que está en index
+    next(error);
     }
 };
 
 // Eliminar una tarea 
-const deleteTask = async(req, res) => {
-    const {id} = req.params;
-    const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
-    console.log(result)
-    if(result.rowCount === 0)
-    return res.status(404).json({
-message: 'Task not found'
-});
+const deleteTask = async(req, res, next) => {
+    try {
+        const {id} = req.params;
+        const result = await pool.query('DELETE FROM task WHERE id = $1', [id]);
+        console.log(result)
+        if(result.rowCount === 0)
+            return res.status(404).json({
+            message: 'Task not found'
+        });
     return res.sendStatus(204);  
+    } catch(error) {
+        next(error);
+    }
 };
 
 // Actualizar una tarea 
-const updateTask = async(req, res) => {
-    const {id} = req.params;
-    const { title, description } = req.body;
-    console.log(id, title, description);
-    const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', [title, description, id]);
-    console.log(result)
-    if(result.rows.length === 0) // esta condición es para cuando una tarea no existe
-    return res.status(404).json({
-        message:"Task not found",
-    });
+const updateTask = async(req, res, next) => {
+    try {
+        const {id} = req.params;
+        const { title, description } = req.body;
+        console.log(id, title, description);
+        const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', [title, description, id]);
+        console.log(result)
+        if(result.rows.length === 0) // esta condición es para cuando una tarea no existe
+            return res.status(404).json({
+            message:"Task not found",
+        });
     return res.json(result.rows[0]);
-};
 
+    } catch(error){
+        next(error);
+    }
+};
 
 module.exports = {
     getAllTasks,
--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json()); //con este cambio exprees va a poder entender los request body 
 app.use(taskRoutes);
+// Cuando creamos rutas usamos req, res
+// Al colocar err indicamos que se espera que está función sea visitada cuando un error ocurra
+app.use((err, req, res, next) => {
+    return res.json({
+        message: err.message
+    })
+})
 
 app.listen(4000)
 console.log('Server on port 4000')
