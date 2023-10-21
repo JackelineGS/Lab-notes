70003c6885d3b2ab09ebf0ec4ef9e3840c68cdfc
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-10-20 20:04:11 -0500

    We advance with the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TaskList from './components/TaskList'
+import TaskForm from './components/TaskForm'
+import { Container } from '@mui/material'
+import Menu from './components/Navbar'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Menu/>
+      <Container>
       <Routes>
-        <Route path='/' element={<TaskList/>}> 
-        </Route>
-      </Routes>
+        < Route path='/' element={<TaskList/>}/> 
+        < Route path='/tasks/new' element= {<TaskForm/>}/>
+        {/* Edit Router*/}
+        </Routes>
+      </Container>
     </BrowserRouter>
   )
 }
