7221f812101de5b6439d7da339fe5e3953457803
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-11-20 22:25:08 -0500

    We add new README

--- src/database/db.sql
+++ src/database/db.sql
@@ -5,3 +5,9 @@ CREATE TABLE task(
     title VARCHAR(255) UNIQUE, 
     description VARCHAR(255) 
 );
+
+CREATE TABLE people(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) UNIQUE,
+    role VARCHAR(255)
+)
